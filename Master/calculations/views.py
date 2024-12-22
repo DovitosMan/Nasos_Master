dd15ed779b95d187826b25fbb7692275fb20bdb4
DovitosMan <kashapov_dmitry@bk.ru>
2024-12-22 13:21:59 +0500

    added kpd

--- Master/calculations/views.py
+++ Master/calculations/views.py
@@ -33,6 +33,22 @@ def wheel_calc(request):
                 'name': 'Приведенный диаметр входа в рабочее колесо 2: ',
                 'value': None,
             },
+            {
+                'name': 'Объемный КПД: ',
+                'value': None,
+            },
+            {
+                'name': 'Гидравлический КПД: ',
+                'value': None,
+            },
+            {
+                'name': 'Внутр. мех. КПД: ',
+                'value': None,
+            },
+            {
+                'name': 'Полный ожидаемый КПД: ',
+                'value': None,
+            },
         ],
         'inputs': [
             {
@@ -78,16 +94,23 @@ def calculations(flow_rate, pressure, speed):
         kw = 0.8 * math.sqrt(pump_speed_coef / 100)
     else:
         kw = 0.635 * (pump_speed_coef / 100) ** (5 / 6)
-    width_in_enter_of_work_wheel = round(kw * (flow_rate / 3600 / speed) ** (1 / 3),4)
+    width_in_enter_of_work_wheel = round(kw * (flow_rate / 3600 / speed) ** (1 / 3), 4)
     # Приведенный диаметр входа в рабочее колесо
     kin = 4.5
-    inner_diam_of_work_wheel_1 = round(kin*(flow_rate/60/speed)**(2/3),4)
+    inner_diam_of_work_wheel_1 = round(kin * (flow_rate / 60 / speed) ** (2 / 3), 4)
     z = 7
     alpha = 0.1
-    v0 = alpha*(flow_rate/3600*speed**2)**(1/3)
-    inner_diam_of_work_wheel_2 = round((4*flow_rate/3600/(math.pi*v0))**(1/2),4)
-    #
-    return pump_speed_coef, outer_diam_of_work_wheel, width_in_enter_of_work_wheel,inner_diam_of_work_wheel_1, inner_diam_of_work_wheel_2
+    v0 = alpha * (flow_rate / 3600 * speed ** 2) ** (1 / 3)
+    inner_diam_of_work_wheel_2 = round((4 * flow_rate / 3600 / (math.pi * v0)) ** (1 / 2), 4)
+    # Предварительная оценка КПД
+    n0 = round((1 + (0.68 / (pump_speed_coef ** (2 / 3)))) ** (-1), 3)
+    if inner_diam_of_work_wheel_1 < inner_diam_of_work_wheel_2:
+        nr = round(1 - (0.42 / (math.log10(inner_diam_of_work_wheel_1 * 1000) - 0.172) ** 2), 3)
+    else:
+        nr = round(1 - (0.42 / (math.log10(inner_diam_of_work_wheel_2 * 1000) - 0.172) ** 2), 3)
+    nm = round((1 + (28.6 / pump_speed_coef) ** 2) ** (-1), 3)
+    n_a = round(n0 * nr * nm, 3)
+    return pump_speed_coef, outer_diam_of_work_wheel, width_in_enter_of_work_wheel, inner_diam_of_work_wheel_1, inner_diam_of_work_wheel_2, n0, nr, nm, n_a
 
 
 def update_context(context, values):
