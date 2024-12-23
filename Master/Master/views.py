1b145018af1a8d5602aae8c8b86b45f1ebed73db
Alexisnovy <149501621+Alexisnovy@users.noreply.github.com>
2024-12-23 19:57:36 +0500

    Пробую упростить home.html
    моя проба закомментирована

--- Master/Master/views.py
+++ Master/Master/views.py
@@ -5,6 +5,40 @@ from django.shortcuts import render
 def home(request):
     context = {
         'title': 'Расчет рабочего колеса',
+        # 'buttons': [
+        #     {
+        #         'value': 'Главное меню',
+        #         'url_name': "{% url 'home' %}",
+        #     },
+        #     {
+        #         'value': 'Расчет колеса',
+        #         'url_name': "{% url 'wheel_calc' %}",
+        #     },
+        #     {
+        #         'value': 'Расчет подвода',
+        #         'url_name': "{% url 'home' %}",
+        #     },
+        #     {
+        #         'value': 'Расчет отвода',
+        #         'url_name': "{% url 'home' %}",
+        #     },
+        #     {
+        #         'value': 'Построение характеристики',
+        #         'url_name': "{% url 'characteristics' %}",
+        #     },
+        #     {
+        #         'value': 'Подбор насоса',
+        #         'url_name': "{% url 'pump_selection' %}",
+        #     },
+        #     {
+        #         'value': 'Расчет энергозатрат',
+        #         'url_name': "{% url 'home' %}",
+        #     },
+        #     {
+        #         'value': 'Расчет трудозатрат',
+        #         'url_name': "{% url 'home' %}",
+        #     },
+        # ],
         'button1': '',
         'button2': 'Расчет колеса',
         'button3': 'Построение характеристики рассчитанного насоса',
