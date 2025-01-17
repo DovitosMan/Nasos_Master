59f1cd129e375d5c522113c04d6537f596c7ce14
DovitosMan <kashapov_dmitry@bk.ru>
2025-01-17 18:46:10 +0500

    удалил лишнее 2

--- Master/Master/views.py
+++ Master/Master/views.py
@@ -5,50 +5,40 @@ from django.shortcuts import render
 def home(request):
     context = {
         'title': 'Расчет рабочего колеса',
-        # 'buttons': [
-        #     {
-        #         'value': 'Главное меню',
-        #         'url_name': "{% url 'home' %}",
-        #     },
-        #     {
-        #         'value': 'Расчет колеса',
-        #         'url_name': "{% url 'wheel_calc' %}",
-        #     },
-        #     {
-        #         'value': 'Расчет подвода',
-        #         'url_name': "{% url 'home' %}",
-        #     },
-        #     {
-        #         'value': 'Расчет отвода',
-        #         'url_name': "{% url 'home' %}",
-        #     },
-        #     {
-        #         'value': 'Построение характеристики',
-        #         'url_name': "{% url 'characteristics' %}",
-        #     },
-        #     {
-        #         'value': 'Подбор насоса',
-        #         'url_name': "{% url 'pump_selection' %}",
-        #     },
-        #     {
-        #         'value': 'Расчет энергозатрат',
-        #         'url_name': "{% url 'home' %}",
-        #     },
-        #     {
-        #         'value': 'Расчет трудозатрат',
-        #         'url_name': "{% url 'home' %}",
-        #     },
-        # ],
-        'button0': 'Pump Master',
-        'button1': 'Главное меню',
-        'button2': 'Расчет колеса',
-        'button3': 'Расчет подвода',
-        'button4': 'Расчет отвода',
-        'button5': 'Построение характеристики',
-        'button6': 'Подбор насоса',
-        'button7': 'Расчет энергозатрат',
-        'button8': 'Расчет трудозатрат',
-        'button9': 'Личный кабинет',
+        'buttons': [
+            {
+                'value': 'Расчет колеса',
+                'url_name': "wheel_calc",
+            },
+            {
+                'value': 'Расчет подвода',
+                'url_name': "home",
+            },
+            {
+                'value': 'Расчет отвода',
+                'url_name': "home",
+            },
+            {
+                'value': 'Построение характеристики',
+                'url_name': "characteristics",
+            },
+            {
+                'value': 'Подбор насоса',
+                'url_name': "pump_selection",
+            },
+            {
+                'value': 'Расчет энергозатрат',
+                'url_name': "home",
+            },
+            {
+                'value': 'Расчет трудозатрат',
+                'url_name': "home",
+            },
+            {
+                'value': 'Личный кабинет',
+                'url_name': "home",
+            },
+        ],
     }
     return render(request, 'home.html', context)
 
