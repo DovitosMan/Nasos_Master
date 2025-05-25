237e7acab96c9b8d9aa34f63f3fc9e7a8fa1e2ea
DovitosMan <kashapov_dmitry@bk.ru>
2025-05-25 20:09:07 +0500

    добавил разворачивание блока приложения

--- Master/static/Master/js/Master.js
+++ Master/static/Master/js/Master.js
@@ -2,20 +2,23 @@
 const menuToggle = document.getElementById('menuToggle');
 const sidebar = document.getElementById('menuSelect');
 const overlay = document.getElementById('overlay');
+const toggle_block = document.getElementById('toggle_block');
+const block_application = document.getElementById('block_application');
 
-
-
-// Задаем такой же сдвиг content как и у sidebar
-// if (contentShift) {
-//     contentShift.style.marginLeft = getComputedStyle(sidebar).left;
-// }
-// Добавляем обработчик события на кнопку
 menuToggle.addEventListener('click', () => {
     // Переключаем классы для меню и контента
     sidebar.classList.toggle('open');
     overlay.classList.toggle('on');
 });
-
+toggle_block.addEventListener('click', () => {
+    //Сворачивание блока приложения
+    block_application.classList.toggle('toggled');
+});
+if (toggle_block.classList.contains('toggled')) {
+  toggle_block.style.maxHeight = '20px';
+} else {
+  toggle_block.style.maxHeight = toggle_block.scrollHeight + 'px';
+}
 
 
 
