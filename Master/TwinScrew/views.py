4f7eac7582cce6de72c42a35060e7b61a2a10173
Alexisnovy <149501621+Alexisnovy@users.noreply.github.com>
2025-07-10 16:01:02 +0500

    Добавил создание моделей винтов

--- Master/TwinScrew/views.py
+++ Master/TwinScrew/views.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 import itertools
 import pandas as pd
 import logging
+import cadquery as cq
+from cadquery import exporters, Compound
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -38,7 +40,7 @@ def twin_screw(request):
             except ValueError:
                 input_data[name] = None
 
-        result = calculate(**input_data)
+        result, model = calculate(**input_data)
 
         print("Лучшие параметры и вычисленные значения:")
 
@@ -48,10 +50,13 @@ def twin_screw(request):
             for key, value in result.items():
                 print(f"{key}: {value}")
 
+        create_bodies(model)
+
     return render(request, 'twinscrew.html', context)
 
 
-def calculate(flow_rate, pressure, rotation_speed, density, viscosity, temperature, heat_capacity, double_inlet=True, num_threads=2):
+def calculate(flow_rate, pressure, rotation_speed, density, viscosity, temperature, heat_capacity, double_inlet=True,
+              num_threads=2):
     def pre_calc(r_ratio, t_ratio, alpha, kpd_vol, flow_rate_add):
         phi = 2 * math.acos((1 + r_ratio) / 2)
 
@@ -137,23 +142,6 @@ def calculate(flow_rate, pressure, rotation_speed, density, viscosity, temperatu
         # Если зазор отрицательный, уменьшать число заходов num
         return stator_gap, screw_gap, side_gap, b_ext_top, b_int_low, b_ext_low, axis_dist
 
-    def calc_liquid_prop(temp):
-        temp_20 = 20
-
-        density_20 = 998.2
-        density_koef = 0.0002
-        density = round(density_20 * (1 - density_koef * (temp - temp_20)), 2)
-
-        viscosity_dyn_20 = 1.003
-        viscosity_dyn_koef = 0.023
-        viscosity_dyn = round(viscosity_dyn_20 * math.exp(-viscosity_dyn_koef * (temp - temp_20)), 3)
-
-        viscosity_kin = round(viscosity_dyn / density * 1000, 3)
-
-        capacity_temp = round(4212 - 3.2 * temp + 0.014 * math.pow(temp, 2), 2)
-
-        return density, viscosity_dyn, viscosity_kin, capacity_temp
-
     flow_rate_adj = flow_rate / 2 if double_inlet else flow_rate
 
     kpd_vol_pre_fixed = 0.8
@@ -191,8 +179,6 @@ def calculate(flow_rate, pressure, rotation_speed, density, viscosity, temperatu
                                                                                                    ext_r, int_r,
                                                                                                    alpha,
                                                                                                    num_threads)
-            # density, viscosity_dyn, viscosity_kin, capacity_temp = calc_liquid_prop(temperature)
-
             viscosity_kin = viscosity / density * 1000
 
             pressure_mpa = 0.009806649643957326 * pressure
@@ -279,8 +265,6 @@ def calculate(flow_rate, pressure, rotation_speed, density, viscosity, temperatu
     delta_t_val = delta_t(theta1_root, r_ratio_b, ext_r, t, alpha_b)
     stator_gap, screw_gap, side_gap, b_ext_top, b_int_low, b_ext_low, axis_dist = gap_calc(delta_t_val, t, ext_r, int_r,
                                                                                            alpha_b, num_threads)
-    # density, viscosity_dyn, viscosity_kin, capacity_temp = calc_liquid_prop(temperature)
-
     viscosity_kin = viscosity / density * 1000
 
     pressure_mpa = 0.009806649643957326 * pressure
@@ -369,6 +353,17 @@ def calculate(flow_rate, pressure, rotation_speed, density, viscosity, temperatu
         "power_required": power_required,
     }
 
+    results_model = {
+        "alpha": float(alpha_b),
+        "ext_radius_mm": float(ext_r),
+        "int_radius_mm": float(int_r),
+        "t_mm": float(t),
+        "axis_dist": float(axis_dist),
+        "thread_length_mm": float(thread_length_mm),
+        "b_ext_top": float(b_ext_top),
+        "b_int_low": float(b_int_low),
+    }
+
     df = pd.DataFrame([results_dict])  # одна строка таблицы
 
     # Имя файла по наружному диаметру
@@ -379,4 +374,76 @@ def calculate(flow_rate, pressure, rotation_speed, density, viscosity, temperatu
     df.to_excel(filename, index=False)
     print(f"Результаты сохранены в файл: {filename}")
 
-    return results_dict
+    return results_dict, results_model
+
+
+def create_bodies(results_model):
+    alpha = results_model['alpha']
+    ext_radius_mm = results_model['ext_radius_mm']
+    int_radius_mm = results_model['int_radius_mm']
+    t_mm = results_model['t_mm']
+    axis_dist = results_model['axis_dist']
+    thread_length_mm = results_model['thread_length_mm']
+    b_ext_top = results_model['b_ext_top']
+    b_int_low = results_model['b_int_low']
+
+    int_circle = cq.Workplane('XY').workplane(offset=-t_mm).circle(int_radius_mm)
+    int_circle_extruded = int_circle.extrude(thread_length_mm + 2 * t_mm)
+
+    shift = 1
+    add = (ext_radius_mm - int_radius_mm) * math.tan(math.radians(alpha))
+    add_shift = shift / math.tan(math.radians(alpha))
+    trapezoid = cq.Workplane('ZY').polyline([
+        (-t_mm - shift, int_radius_mm - add_shift),
+        (-t_mm + add + shift, ext_radius_mm + add_shift),
+        (-t_mm + add + b_ext_top - shift, ext_radius_mm + add_shift),
+        (-t_mm + 2 * add + b_ext_top + shift, int_radius_mm - add_shift)
+    ]).close()
+
+    spiral = cq.Wire.makeHelix(
+        pitch=t_mm,
+        height=thread_length_mm + 2 * t_mm,
+        radius=ext_radius_mm,
+        lefthand=False,
+        center=(0, 0, -t_mm),
+        dir=(0, 0, 1)
+    )
+
+    spiral_body_1 = trapezoid.sweep(spiral, isFrenet=True)
+    spiral_body_2 = spiral_body_1.rotate((0, 0, 0), (0, 0, 1), 180)
+
+    ext_circle = cq.Workplane('XY').workplane(offset=-2 * t_mm).circle(ext_radius_mm).circle(ext_radius_mm * 1.5)
+    ext_circle_extruded = ext_circle.extrude(thread_length_mm + 4 * t_mm)
+
+    circle_cut_1 = cq.Workplane('XY').circle(2 * ext_radius_mm)
+    circle_cut_2 = cq.Workplane('XY').workplane(offset=thread_length_mm).circle(2 * ext_radius_mm)
+    circle_cut_1_extruded = circle_cut_1.extrude(-thread_length_mm)
+    circle_cut_2_extruded = circle_cut_2.extrude(thread_length_mm)
+
+    body_1 = (
+        int_circle_extruded.
+        union(spiral_body_1).
+        union(spiral_body_2).
+        cut(ext_circle_extruded).
+        cut(circle_cut_1_extruded).
+        cut(circle_cut_2_extruded)
+    )
+
+    rotate_dist = b_int_low + add
+    rotate_angle = 360 * rotate_dist / t_mm
+
+    body_2 = body_1.mirror('YZ').rotate((0, 0, 0), (0, 0, 1), angleDegrees=rotate_angle)
+
+    exporters.export(body_1, 'first_screw.step')
+    logger.info('Первый вал успешно создан и экспортирован в first_screw.step')
+    exporters.export(body_2, 'second_screw.step')
+    logger.info('Второй вал успешно создан и экспортирован в second_screw.step')
+
+    first_screw = cq.importers.importStep('first_screw.step')
+    second_screw = cq.importers.importStep('second_screw.step')
+
+    assembly = cq.Assembly()
+    assembly.add(first_screw, name='first_screw', loc=cq.Location(cq.Vector(0, axis_dist / 2, 0)))
+    assembly.add(second_screw, name='second_screw', loc=cq.Location(cq.Vector(0, -axis_dist / 2, 0)))
+    exporters.export(assembly.toCompound(), 'twin_assembly.step')
+    logger.info('Сборка успешно создана и экспортирована в twin_assembly.step')
